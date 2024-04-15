373774e6ec7f5c339ebd35755a263ec8f8e9da4b
Maximilian Steinert <71747937+iradraconis@users.noreply.github.com>
2024-04-15 10:55:16 +0200

    fixed bug in shell script

--- Posteingang.sh
+++ Posteingang.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Der Pfad zum aktuellen Script
+SCRIPT_PATH="$(dirname "$0")"
+
+# Ausgabe des aktuellen Pfads für Klarheit
+echo "Das Script befindet sich in: $SCRIPT_PATH"
+
+# Wechselt in das Verzeichnis, in dem das Script liegt
+cd "$SCRIPT_PATH"
+
+# Bestätigung des aktuellen Verzeichnisses nach dem Wechsel
+echo "Aktuelles Verzeichnis: $(pwd)"
+
 echo "Post wird abgeholt, konvertiert und in Scan-Eingang verschoben"
 #cd /home/$USER/PycharmProjects/email-ocr-pdf-merge.py/
 
